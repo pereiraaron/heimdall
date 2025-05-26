378e65adc3f08675cc4f72f793c30c2f2f48a98a
Aron Pereira <aronpereira1999@gmail.com>
2025-05-26 13:08:22 +0530

    Added register controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import { User } from "../models";
+import bcrypt from "bcrypt";
 
 export const login = async (req: Request, res: Response) => {};
 
-export const register = async (req: Request, res: Response) => {};
+export const register = async (req: Request, res: Response) => {
+  const { username, password, role } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: `User registered with ${username}` });
+  } catch (error) {
+    res.status(500).json({ message: "Registration failed", error: error });
+  }
+};
 
 export const getProfile = async (req: Request, res: Response) => {};
