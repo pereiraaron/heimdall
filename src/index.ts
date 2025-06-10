a1865e15c050cc26275331fe4ba3985f8e660456
Aron Pereira <aronpereira1999@gmail.com>
2025-06-10 11:23:20 +0530

    Added deployment crash fix

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import { connectToDB } from "./db/connect";
 import authRoutes from "./routes/auth";
+import userRoutes from "./routes/user";
 
 dotenv.config();
+
 const app: Express = express();
 const PORT = process.env.PORT || 7001;
 
-//Middleware
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // Health check route
 app.get("/", (_, res) => {
   res.status(200).json({ message: "Heimdall is guarding your API!" });
 });
 
-connectToDB().then(() => {
+connectToDB();
+
+if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Heimdall is guarding on port ${PORT}`);
   });
-});
+}
+
+export default app;
